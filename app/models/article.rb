390e4bf573da511faab83162c51fc2a11b26e226
EmieD17 <Doucet.emie@gmail.com>
2022-01-20 11:48:06 -0500

    image working

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,9 @@ class Article < ApplicationRecord
   include Visible
 
   has_many :comments, dependent: :destroy
+  has_one_attached :image
 
   validates :title, presence: true
+  validates :image, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 end
